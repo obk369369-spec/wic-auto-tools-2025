2ed559aad987a6f9577afcb3e9586690258982a7
obk369369-spec <obk369369@gmail.com>
2025-10-14 23:17:18 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
-// app/layout.tsx
+export const metadata = { title: "wic-auto-tools-2025" };
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body>{children}</body>
     </html>
-  )
+  );
 }
